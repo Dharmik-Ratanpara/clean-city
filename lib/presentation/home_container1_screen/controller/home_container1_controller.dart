2c42f57d01a4a8e661ac7ad15878696932db1ca6
Dharmik Ratanpara <dharmikratanpara25@gmail.com>
2023-06-26 17:42:00 +0530

    Firebase Configuration added

--- lib/presentation/home_container1_screen/controller/home_container1_controller.dart
+++ lib/presentation/home_container1_screen/controller/home_container1_controller.dart
@@ -1,6 +1,5 @@
 import 'package:clean_city/core/app_export.dart';
 import 'package:clean_city/presentation/home_container1_screen/models/home_container1_model.dart';
-import 'package:clean_city/widgets/custom_bottom_bar.dart';
 
 class HomeContainer1Controller extends GetxController {
   Rx<HomeContainer1Model> homeContainer1ModelObj = HomeContainer1Model().obs;
